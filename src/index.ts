68cfae7e6f63336c11d244b6533ce5811472caa2
Marcelo Adamatti <adamatti@gmail.com>
2024-09-11 00:08:14 -0300

    chore: added AI

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import { Hono } from "hono";
 
+const model: BaseAiTextGenerationModels =
+	"@cf/mistral/mistral-7b-instruct-v0.1";
 const app = new Hono<{ Bindings: Env }>();
 
 app.get("/", (c) => {
 	return c.text("Hello World!");
+});
+
+app.get("/ai", async (c) => {
+	const prompt = c.req.query("q") ?? "What can you do today?";
+	const answer: AiTextGenerationOutput = await c.env.AI.run(model, {
+		prompt,
+		stream: false,
+	});
+	return c.json(answer);
 });
 
 export default app;
